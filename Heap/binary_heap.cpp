68c971358a9e12740cd76eade6d083305fc73470
Abhijit Ganguly <105243912+Abhijit-011@users.noreply.github.com>
2022-10-07 23:25:51 +0530

    Add files via upload

---
+++ Heap/binary_heap.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void swap(int *x,int* y);
+
+class heap
+{
+    int *head;
+    int cap;
+    int size;
+public:
+    minheap(int cap);
+
+    int parent(int i){return (i-1)/2;}
+    int left(int i){return (2*i +1);}
+    int right(int i){return (2*i +2);}
+
+    int extractmin();
+
+    void decreasekey(int i,int new_val);
+
+    int getmin(){return head[0];}
+
+    void delete_key(int i);
+    void insert(int k);
+};
+heap::minheap(int cap){
+
+}
+
+
+int main(){
+
+return 0;
+}
