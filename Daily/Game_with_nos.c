749c09722ee9dc914be1ac1de76b33e24a606e06
Abhijit Ganguly <105243912+Abhijit-011@users.noreply.github.com>
2023-10-03 06:27:03 +0530

    Update Game_with_nos.c

--- Daily/Game_with_nos.c
+++ Daily/Game_with_nos.c
@@ -50,7 +50,7 @@ int* game_with_number(int arr[], int n)
     // Complete the function
     for(int i=0;i<n-1;i++)
     {
-        arr[i]=arr[i]^arr[i+1];
+        //help me to solve
     }
     
     return arr;
