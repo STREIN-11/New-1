6928fbfdd0f8d134927712b635734d67dc0dc049
Abhijit Ganguly <105243912+Abhijit-011@users.noreply.github.com>
2023-10-03 06:21:50 +0530

    Update x_total_shape.cpp

--- Daily/x_total_shape.cpp
+++ Daily/x_total_shape.cpp
@@ -29,13 +29,12 @@ class Solution
         m = grid[0].size();
         int ans = 0;
 	for(int i=0;i<n;i++){
-            for(int j=0; j<m;j++){
                 if(grid[i][j]=='X'){
                     ans++;
                     dfs(i,j,grid);
                 }
             }
-        }
+        
         
         
       
